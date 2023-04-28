1041509047d56056842079d63dc87fa5f091179e
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-28 12:37:14 -0600

    feat: Archivo principal de la app

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:unica_cybercoffee/ui/providers/theme_provider.dart';
+import 'package:unica_cybercoffee/ui/unica_cybercoffee.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() {
+  setPathUrlStrategy();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  ThemeProvider themeChangeProvider = ThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAppTheme();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setTheme =
+        await themeChangeProvider.themePreference.getTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: themeChangeProvider,
+      child: const UnicaCyberCoffe(),
+    );
+  }
+}
