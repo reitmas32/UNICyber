bdf7cd96fa1be2c6e9e5932eebba50542cd6170a
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-28 12:38:14 -0600

    feat: Aplicacion UnicaCyberCoffee

---
+++ lib/ui/unica_cybercoffee.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:unica_cybercoffee/ui/pages/login_page.dart';
+import 'package:unica_cybercoffee/ui/pages/splash_page/splash_page.dart';
+import 'package:unica_cybercoffee/ui/providers/theme_provider.dart';
+import 'package:unica_cybercoffee/ui/theme_preference.dart';
+import 'package:provider/provider.dart';
+
+class UnicaCyberCoffe extends StatefulWidget {
+  const UnicaCyberCoffe({
+    super.key,
+  });
+
+  @override
+  State<UnicaCyberCoffe> createState() => UnicaCyberCoffeState();
+}
+
+class UnicaCyberCoffeState extends State<UnicaCyberCoffe> {
+  final GoRouter _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => const SplashScreen(),
+      ),
+      GoRoute(
+        path: "/login",
+        builder: (context, state) => const LoginPage(
+          title: 'Home',
+        ),
+      ),
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final currentTheme = Provider.of<ThemeProvider>(context);
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+      theme:
+          currentTheme.isDarkTheme() ? AppTheme.darkTheme : AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.system,
+    );
+  }
+}
