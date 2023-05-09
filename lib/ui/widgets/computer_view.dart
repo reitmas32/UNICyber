4729eb4c782245fb1c50b3f3b8c6e38fffdcde4c
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-09 16:31:02 -0600

    feat: Create ComputerView

---
+++ lib/ui/widgets/computer_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ComputerView extends StatelessWidget {
+  const ComputerView({
+    super.key,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  final String name;
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      height: 120,
+      child: Column(
+        children: [
+          Text(name),
+          Image.network(
+            imageUrl,
+          ),
+        ],
+      ),
+    );
+  }
+}
