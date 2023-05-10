58ec748277395201e5d25d3ece233046ba6011a5
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-10 16:35:57 -0600

    feat: Add DropMenus

--- lib/ui/widgets/dialogs/computer_info_dialog.dart
+++ lib/ui/widgets/dialogs/computer_info_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class ComputerInfoDialog extends StatelessWidget {
@@ -11,11 +12,68 @@ class ComputerInfoDialog extends StatelessWidget {
       title: const Text('Datos de la Computadora'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
-        children: const [
-          Text('Disponible'),
-          Text('Estado'),
-          Text('Usuario'),
-          Text('Informacion'),
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: InkWell(
+              borderRadius: BorderRadius.circular(10.0),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              hoverColor: Theme.of(context).colorScheme.secondary,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(25.0),
+                  child: const Text(
+                    'Disponible',
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'Estado',
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          const MyDropdownMenu(
+            items: [
+              'Descompuesto',
+              'Mantenimiento',
+              'Reparación',
+              'Proyecto',
+              'Impresión',
+            ],
+          ),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'Usuario',
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          const MyDropdownMenu(
+            items: [
+              'Info Usuario',
+              'Sancionar',
+            ],
+          ),
         ],
       ),
       actions: [
@@ -26,14 +84,60 @@ class ComputerInfoDialog extends StatelessWidget {
           },
           hoverColor: Theme.of(context).colorScheme.secondary,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(25.0),
-              child: const Text('Guardar'),
+              child: const Text(
+                'Guardar',
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
             ),
           ),
         ),
       ],
+    );
+  }
+}
+
+class MyDropdownMenu extends StatefulWidget {
+  const MyDropdownMenu({super.key, required this.items});
+
+  final List<String> items;
+
+  @override
+  _MyDropdownMenuState createState() => _MyDropdownMenuState();
+}
+
+class _MyDropdownMenuState extends State<MyDropdownMenu> {
+  String _selectedItem = '';
+
+  @override
+  void initState() {
+    _selectedItem = widget.items.first;
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      borderRadius: BorderRadius.circular(5.0),
+      elevation: 5,
+      hint: const Text('Selecciona una opción'),
+      value: _selectedItem,
+      onChanged: (newValue) {
+        setState(() {
+          _selectedItem = newValue ?? widget.items.first;
+        });
+      },
+      items: widget.items.map((item) {
+        return DropdownMenuItem(
+          value: item,
+          child: Text(item),
+        );
+      }).toList(),
     );
   }
 }
