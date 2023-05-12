b6ccc7ce1c4b9155c7a393fd01e8da96f9e536f2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-12 01:48:08 -0600

    feat: Add idUserAdmin

--- lib/domain/models/computer_room.dart
+++ lib/domain/models/computer_room.dart
@@ -1,21 +1,22 @@
 
 class ComputerRoomUI{
   late String id;
+  String idUserAdmin;
   String name;
 
-  ComputerRoomUI({this.id = 'dash6854hyabdys', required this.name});
+  ComputerRoomUI({this.id = 'dash6854hyabdys', required this.name, required this.idUserAdmin});
 
   static ComputerRoomUI fromMap(Map<dynamic, dynamic> data){
-    return ComputerRoomUI(id: data['id'], name: data['name']);
+    return ComputerRoomUI(id: data['id'], name: data['name'], idUserAdmin: data['idUserAdmin']);
   }
 
   Map<dynamic, dynamic> toMap(){
 
-    return {'id': id, 'name': name};
+    return {'id': id, 'name': name, 'idUserAdmin': idUserAdmin};
   }
 
     Map<String, dynamic> toJson(){
 
-    return {'id': id, 'name': name};
+    return {'id': id, 'name': name, 'idUserAdmin': idUserAdmin};
   }
 }
