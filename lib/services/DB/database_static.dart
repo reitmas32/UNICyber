bc25932b4e1cc89f53ee973563396cbdeb2d8df0
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-12 01:48:35 -0600

    feat: Create DataBaseStatic

--- lib/services/DB/database_static.dart
+++ lib/services/DB/database_static.dart
@@ -2,49 +2,40 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:path_provider/path_provider.dart';
-import 'package:unica_cybercoffee/domain/models/computerUI.dart';
-import 'package:unica_cybercoffee/domain/models/computer_room.dart';
-import 'package:unica_cybercoffee/services/DB/idatabase_UI.dart';
+import 'package:unica_cybercoffee/domain/models/user_admin.dart';
 import 'package:unica_cybercoffee/tools/randomID.dart';
 
-class DataBaseStaticUI extends IDataBaseUI {
-  List<ComputerUI> computers = [];
-  List<ComputerRoomUI> computerRooms = [];
+class DataBaseStatic {
+  List<UserAdmin> usersAdmin = [];
+  UserAdmin activeUserAdmin = UserAdmin(userName: '', password: '', id: '');
+  //List<ComputerRoomUI> computerRooms = [];
 
   Future<bool> loadData() async {
     final directory = await getApplicationSupportDirectory();
-    final collectionComputersFile =
-        File('${directory.path}/collection_computers.json');
-    if (!await collectionComputersFile.exists()) {
-      await collectionComputersFile.create();
-    } else {
-      final collectionComputersJson =
-          collectionComputersFile.readAsStringSync();
-      if (!collectionComputersJson.isEmpty) {
-        final List<dynamic> collectionComputersList =
-            jsonDecode(collectionComputersJson);
-        computers = collectionComputersList
-            .map((json) => ComputerUI.fromMap(json))
-            .toList();
-      }
-    }
 
-    final collectionComputerRoomsFile =
-        File('${directory.path}/collection_computer_rooms.json');
-    if (!await collectionComputerRoomsFile.exists()) {
-      await collectionComputerRoomsFile.create();
+    //Get Dir of File
+    final collectionUsersAdmin = File('${directory.path}/users_admin.json');
+
+    //If Not Exist Create
+    if (!await collectionUsersAdmin.exists()) {
+      await collectionUsersAdmin.create();
     } else {
-      final collectionComputerRoomsJson =
-          collectionComputerRoomsFile.readAsStringSync();
-      if (!collectionComputerRoomsJson.isEmpty) {
-        final List<dynamic> collectionComputerRoomsList =
-            jsonDecode(collectionComputerRoomsJson);
-        computerRooms = collectionComputerRoomsList
-            .map((json) => ComputerRoomUI.fromMap(json))
+      //Read Data
+      final collectionUsersAdminJson = collectionUsersAdmin.readAsStringSync();
+
+      //If Exist Data
+      if (collectionUsersAdminJson.isNotEmpty) {
+        //Load Data
+        final List<dynamic> collectionUsersAdminList =
+            jsonDecode(collectionUsersAdminJson);
+
+        //Decode Data
+        usersAdmin = collectionUsersAdminList
+            .map(
+              (json) => UserAdmin.fromJson(json),
+            )
             .toList();
-      }else{
-        await createComputerRooms('Nueva Aula');
-      }
+      } else {}
     }
 
     return Future(() => true);
@@ -52,84 +43,50 @@ class DataBaseStaticUI extends IDataBaseUI {
 
   Future<bool> saveData() async {
     final directory = await getApplicationSupportDirectory();
-    final collectionComputers =
-        File('${directory.path}/collection_computers.json');
-    final sinkCollectionComputers = collectionComputers.openWrite();
 
-    final collectionComputerRooms =
-        File('${directory.path}/collection_computer_rooms.json');
-    final sinkCollectionComputerRooms = collectionComputerRooms.openWrite();
+    //Get File
+    final collectionUsersAdmin = File('${directory.path}/users_admin.json');
 
-    sinkCollectionComputers.write(
-        jsonEncode(computers.map((computer) => computer.toJson()).toList()));
-    sinkCollectionComputerRooms.write(jsonEncode(
-        computerRooms.map((computer) => computer.toJson()).toList()));
+    //OpenFile to Write
+    final sinkCollectionUsersAdmin = collectionUsersAdmin.openWrite();
 
-    await sinkCollectionComputers.flush();
-    await sinkCollectionComputers.close();
+    // Write File
+    sinkCollectionUsersAdmin.write(
+        jsonEncode(usersAdmin.map((computer) => computer.toJson()).toList()));
+
+    //Close Data
+    await sinkCollectionUsersAdmin.flush();
+    await sinkCollectionUsersAdmin.close();
+
     return Future(() => true);
   }
 
-  @override
-  Future<ComputerUI> createComputer(
-      String nameRoom, String nameComputer) async {
+  Future<UserAdmin> signupUserAdmin(String userName, String password) async {
     final randomID = randomString();
-    var computerRoom = await findComputerRooms(nameRoom);
-    var computer = ComputerUI(
-      name: nameComputer,
-      imageUrl: 'imageUrl',
-      state: 'state',
+    var userAdmin = UserAdmin(
+      userName: userName,
+      password: password,
       id: randomID,
-      x: 0,
-      y: 0,
-      idComputerRoom: computerRoom.id,
     );
-    computers.add(
-      computer,
+    usersAdmin.add(
+      userAdmin,
     );
 
-    return Future(() => computer);
+    print(usersAdmin);
+
+    return Future(() => userAdmin);
   }
 
-  @override
-  Future<bool> deleteComputer(String nameComputer) async {
+  Future<bool> signinUserAdmin(String userName, String password) async {
+    for(UserAdmin userAdmin in usersAdmin){
+      if(userAdmin.userName == userName && userAdmin.password == password){
+        activeUserAdmin = userAdmin;
+        return true;
+      }
+    }
+
     return Future(() => false);
-  }
-
-  @override
-  Future<List<ComputerUI>> getComputers() async {
-    // TODO: implement getComputers
-    return computers;
-  }
-
-  @override
-  Future<String> createComputerRooms(String nameRoom) async {
-    final randomID = randomString();
-    computerRooms.add(
-      ComputerRoomUI(id: randomID, name: nameRoom),
-    );
-    return Future(() => randomID);
-  }
-
-  @override
-  Future<ComputerRoomUI> findComputerRooms(String nameRoom) async {
-    ComputerRoomUI result = computerRooms.firstWhere(
-        (computerRoom) => computerRoom.name == nameRoom,
-        orElse: () => ComputerRoomUI(name: 'None'));
-
-    return result;
-  }
-
-  @override
-  Future<List<ComputerRoomUI>> getComputerRooms() async {
-    return Future(() => computerRooms);
-  }
-
-  @override
-  Future<bool> deleteComputerRooms(String nameRoom) async {
-    computerRooms.removeLast();
-    return Future(() => true);
   }
 }
 
-DataBaseStaticUI databaseUI_Static = DataBaseStaticUI();
+DataBaseStatic databaseStatic = DataBaseStatic();
