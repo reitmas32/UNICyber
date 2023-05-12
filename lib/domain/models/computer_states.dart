6cd9b309780c3ec071eeb09b9f56070c78e63ce3
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-12 03:38:05 -0600

    feat: CReate States Of Computers

---
+++ lib/domain/models/computer_states.dart
@@ -0,0 +1,6 @@
+class ComputerStates {
+  static String disponible = 'Disponible';
+  static String mantenimiento = 'Mantenimiento';
+  static String reparacion = 'Reparación';
+  static String proyecto = 'Proyecto';
+}
