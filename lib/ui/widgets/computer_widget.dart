86a734b558b55eb7b5729d5041a31245fdfadb57
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-12 03:38:49 -0600

    feat: CReate States Of Computers

--- lib/ui/widgets/computer_widget.dart
+++ lib/ui/widgets/computer_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:unica_cybercoffee/domain/models/computerUI.dart';
+import 'package:unica_cybercoffee/domain/models/computer_states.dart';
+import 'package:unica_cybercoffee/services/DB/databaseUI_static.dart';
 import 'package:unica_cybercoffee/ui/providers/editable_ui_provider.dart';
 import 'package:unica_cybercoffee/ui/widgets/dialogs/computer_info_dialog.dart';
 import 'package:unica_cybercoffee/ui/widgets/computer_view.dart';
@@ -17,6 +19,18 @@ class ComputerWidget extends StatefulWidget {
 }
 
 class _ComputerWidgetState extends State<ComputerWidget> {
+  String _imageUrl =
+      'https://em-content.zobj.net/source/microsoft-teams/337/desktop-computer_1f5a5-fe0f.png';
+
+  String _imageUrlDisponible =
+      'https://em-content.zobj.net/source/microsoft-teams/337/desktop-computer_1f5a5-fe0f.png';
+  String _imageUrlReparacion =
+      'https://raw.githubusercontent.com/reitmas32/unica_cybercoffee/main/public/assets/desktop-computer-reparacion.png';
+  String _imageUrlProyecto =
+      'https://raw.githubusercontent.com/reitmas32/unica_cybercoffee/main/public/assets/desktop-computer-proyecto.png';
+  String _imageUrlMantenimiento =
+      'https://raw.githubusercontent.com/reitmas32/unica_cybercoffee/main/public/assets/desktop-computer-mantenimiento.png';
+
   @override
   Widget build(BuildContext context) {
     final editableProvider = Provider.of<EditableUIProvider>(context);
@@ -53,7 +67,27 @@ class _ComputerWidgetState extends State<ComputerWidget> {
               },
               child: const ComputerInfoDialog(),
             ),
-          );
+          ).then((value) {
+            setState(() {
+              var state = ComputerStates.disponible;
+              switch (value['state']) {
+                case 0:
+                  state = ComputerStates.disponible;
+                  break;
+                case 1:
+                  state = ComputerStates.mantenimiento;
+                  break;
+                case 2:
+                  state = ComputerStates.reparacion;
+                  break;
+                case 3:
+                  state = ComputerStates.proyecto;
+                  break;
+                default:
+              }
+              databaseUI_Static.setStateComputer(widget.computer.id, state);
+            });
+          });
         },
         onPanUpdate: (details) {
           if (editableProvider.editable) {
@@ -65,8 +99,7 @@ class _ComputerWidgetState extends State<ComputerWidget> {
         },
         child: ComputerView(
           name: widget.computer.name,
-          imageUrl:
-              'https://em-content.zobj.net/source/microsoft-teams/337/desktop-computer_1f5a5-fe0f.png',
+          imageUrl: widget.computer.imageUrl,
         ),
       ),
     );
