63eed7fec3f513048e7cbf6c1697cd3c7524acf5
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-14 00:32:16 -0600

    feat: Add CustomTextFileds and  use RawKeyboardListener

--- lib/ui/widgets/dialogs/loan_computer_dialog.dart
+++ lib/ui/widgets/dialogs/loan_computer_dialog.dart
@@ -1,41 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:unica_cybercoffee/ui/widgets/custom_textfield.dart';
 
-class LoanComputerDialog extends StatelessWidget {
+class LoanComputerDialog extends StatefulWidget {
   LoanComputerDialog({
     super.key,
   });
 
-  final FocusNode nameFocusNode = FocusNode();
+  @override
+  State<LoanComputerDialog> createState() => _LoanComputerDialogState();
+}
+
+class _LoanComputerDialogState extends State<LoanComputerDialog> {
+  TextEditingController accountNumberController =
+      TextEditingController(text: '');
+  TextMaskController maskController = TextMaskController(lengthMask: 1);
+
+  final FocusNode focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    nameFocusNode.requestFocus();
-    return AlertDialog(
-      title: const Text('Numero de cuenta'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            focusNode: nameFocusNode,
+    focusNode.requestFocus();
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKey: (RawKeyEvent event) {
+        if (event.logicalKey == LogicalKeyboardKey.enter) {
+          //widget.onTap();
+        }
+      },
+      child: AlertDialog(
+        title: const Text('Numero de cuenta'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomTextFileds(
+              focusNode: focusNode,
+              indexTextField: 0,
+              textEditingController: accountNumberController,
+              maskController: maskController,
+              lable: 'AccountNumber',
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+            ),
+          ],
+        ),
+        actions: [
+          InkWell(
+            borderRadius: BorderRadius.circular(10.0),
+            onTap: () {
+              Navigator.of(context).pop();
+            },
+            hoverColor: Theme.of(context).colorScheme.secondary,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(25.0),
+                child: const Text('Confirmar'),
+              ),
+            ),
           ),
         ],
       ),
-      actions: [
-        InkWell(
-          borderRadius: BorderRadius.circular(10.0),
-          onTap: () {
-            Navigator.of(context).pop();
-          },
-          hoverColor: Theme.of(context).colorScheme.secondary,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(25.0),
-              child: const Text('Confirmar'),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
