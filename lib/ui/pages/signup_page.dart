ac0b797a9e901d3ebc45d76aaa6795ccd4e76064
reitmas32 <rafa.zamora.rals@gmail.com>
2023-05-14 00:36:20 -0600

    feat: Add CustomTextFileds and  use RawKeyboardListener

--- lib/ui/pages/signup_page.dart
+++ lib/ui/pages/signup_page.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:unica_cybercoffee/services/DB/databaseUI_static.dart';
 import 'package:unica_cybercoffee/services/DB/database_static.dart';
+import 'package:unica_cybercoffee/ui/widgets/appbar/unicaAppBar.dart';
+import 'package:unica_cybercoffee/ui/widgets/custom_textfield.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key, required this.title});
@@ -13,89 +18,106 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-
   TextEditingController userNameController = TextEditingController(text: '');
   TextEditingController passwordController = TextEditingController(text: '');
+  TextMaskController maskController = TextMaskController(lengthMask: 2);
 
+  final FocusNode focusNode = FocusNode();
+
+  @override
+  void initState() {
+    maskController.addListener(() {
+      setState(() {});
+    });
+    setState(() {
+      maskController.updateMask(0);
+      focusNode.requestFocus();
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      //appBar: const PortfolioAppBar(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 60.0),
-              child: Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 150,
-                  /*decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),*/
-                  child: Image.network(
-                      'https://raw.githubusercontent.com/reitmas32/unica_cybercoffee/main/public/assets/unica_logo.jpeg'),
+      appBar: const UnicaAppBar(route: '/'),
+      body: RawKeyboardListener(
+        focusNode: FocusNode(),
+        autofocus: true,
+        onKey: (RawKeyEvent event) {
+          if (event.logicalKey == LogicalKeyboardKey.enter) {
+            onSignin();
+          }
+        },
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: Center(
+                  child: SizedBox(
+                    width: 200,
+                    height: 150,
+                    child: Image.network(
+                        'https://raw.githubusercontent.com/reitmas32/unica_cybercoffee/main/public/assets/unica_logo.jpeg'),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: size.width / 3),
-              child: TextField(
-                controller: userNameController,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'UserName',
-                    hintText: 'Enter valid userName'),
+              CustomTextFileds(
+                focusNode: focusNode,
+                indexTextField: 0,
+                textEditingController: userNameController,
+                maskController: maskController,
+                lable: 'UserName',
+                padding: EdgeInsets.symmetric(
+                    horizontal: size.width / 3, vertical: 16.0),
               ),
-            ),
-            Padding(
-                            padding: EdgeInsets.symmetric(horizontal: size.width / 3),
-
-              //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
-                controller: passwordController,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
+              CustomTextFileds(
+                indexTextField: 1,
+                textEditingController: passwordController,
+                maskController: maskController,
+                lable: 'Password',
+                padding: EdgeInsets.symmetric(
+                    horizontal: size.width / 3, vertical: 16.0),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
-            Container(
-              height: 50,
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-              child: TextButton(
-                onPressed: () async {
-                  await databaseStatic.signupUserAdmin(userNameController.text, passwordController.text);
-                  await databaseStatic.saveData();
-                  await databaseUI_Static.loadData();
-                  context.go('/login');
+              TextButton(
+                onPressed: () {
+                  //TODO FORGOT PASSWORD SCREEN GOES HERE
                 },
                 child: const Text(
-                  'SignIn',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.blue, fontSize: 15),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 130,
-            ),
-          ],
+              Container(
+                height: 50,
+                width: 250,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20)),
+                child: TextButton(
+                  onPressed: onSignin,
+                  child: const Text(
+                    'SignIn',
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 130,
+              ),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  onSignin() async {
+    await databaseStatic.signupUserAdmin(
+        userNameController.text, passwordController.text);
+    await databaseStatic.saveData();
+    await databaseUI_Static.loadData();
+    context.go('/login');
   }
 }
