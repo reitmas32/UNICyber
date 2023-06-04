8ca414bbde12e69be4863e59868c465bce9b4040
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 09:28:12 -0600

    feat: Add Service by Accounts

---
+++ lib/services/API/account.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:unica_cybercoffee/domain/models/user.dart';
+import 'package:unica_cybercoffee/services/API/data_static.dart';
+
+Future<bool> signUp(User user) async {
+  var url = Uri.parse('http://localhost:3000/api/v1/signup');
+  var body = jsonEncode(user.toJson());
+  var response = await http.post(url, body: body);
+  if (response.statusCode == 200) {
+    // La petición fue exitosa, extraer los datos del cuerpo de la respuesta
+    var responseData = jsonDecode(response.body);
+
+    // Acceder a los datos específicos dentro del objeto responseData
+    var success = responseData['Success'];
+
+    return Future(() => success);
+  }
+  return Future(() => false);
+}
+
+Future<bool> signIn(User user) async {
+  var url = Uri.parse('http://localhost:3000/api/v1/signin');
+  var body = jsonEncode(user.toJson());
+  var response = await http.put(url, body: body);
+  if (response.statusCode == 200) {
+    // La petición fue exitosa, extraer los datos del cuerpo de la respuesta
+    var responseData = jsonDecode(response.body);
+
+    // Acceder a los datos específicos dentro del objeto responseData
+    var success = responseData['Success'];
+
+    if (success) {
+      dataStatic.jwt = responseData['Data']['token_jwt'];
+    }
+
+    return Future(() => success);
+  }
+  return Future(() => false);
+}
