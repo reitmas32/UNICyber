f9be78812b60842d0a257f6301b193005006e587
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-04 21:58:14 -0600

    feat: Add member to DataStatics

--- lib/services/API/data_static.dart
+++ lib/services/API/data_static.dart
@@ -1,5 +1,8 @@
+import 'package:unica_cybercoffee/domain/models/computer_lab.dart';
+
 class DataStatic {
   String jwt;
+  List<ComputerLab> allComputerLabs = [];
 
   DataStatic({this.jwt = ''});
 }
