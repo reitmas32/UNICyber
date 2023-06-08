6549d3f3327b3223adfaf5c2fcec117b312892e2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-08 16:41:25 -0600

    feat: add fields to DataStatic

--- lib/services/API/data_static.dart
+++ lib/services/API/data_static.dart
@@ -1,11 +1,21 @@
+import 'package:unica_cybercoffee/domain/models/computer.dart';
 import 'package:unica_cybercoffee/domain/models/computer_lab.dart';
+import 'package:unica_cybercoffee/domain/models/room.dart';
 
 class DataStatic {
   String jwt;
   List<ComputerLab> allComputerLabs = [];
+  List<Room> roomsOfComputerLab = [];
+  List<Computer> computerOfRoom = [];
   String userName;
+  int idComputerLab;
+  int idRoomCurrent;
 
-  DataStatic({this.jwt = '', this.userName = ''});
+  DataStatic(
+      {this.jwt = '',
+      this.userName = '',
+      this.idComputerLab = -1,
+      this.idRoomCurrent = -1});
 }
 
 DataStatic dataStatic = DataStatic();
