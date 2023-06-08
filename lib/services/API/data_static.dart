a3e8c8f3c4ec5106705473da4d4c5ab899c1d684
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-08 10:16:53 -0600

    feat: add userName field

--- lib/services/API/data_static.dart
+++ lib/services/API/data_static.dart
@@ -3,8 +3,9 @@ import 'package:unica_cybercoffee/domain/models/computer_lab.dart';
 class DataStatic {
   String jwt;
   List<ComputerLab> allComputerLabs = [];
+  String userName;
 
-  DataStatic({this.jwt = ''});
+  DataStatic({this.jwt = '', this.userName = ''});
 }
 
 DataStatic dataStatic = DataStatic();
