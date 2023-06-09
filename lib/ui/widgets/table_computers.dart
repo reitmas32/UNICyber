091dc7f7273834234f0f391dd95f4481ea13fedd
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-08 20:33:04 -0600

    feat: add CircularProgressIndicator before show computers

--- lib/ui/widgets/table_computers.dart
+++ lib/ui/widgets/table_computers.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:unica_cybercoffee/domain/models/computer.dart';
 import 'package:unica_cybercoffee/domain/models/computer_ui.dart';
+import 'package:unica_cybercoffee/ui/providers/editable_ui_provider.dart';
 import 'package:unica_cybercoffee/ui/widgets/computer_widget.dart';
 
 class TableComputers extends StatefulWidget {
-  const TableComputers({super.key, required this.computers});
+  const TableComputers(
+      {super.key, required this.computers, required this.editableUIProvider});
   final List<Computer> computers;
+  final EditableUIProvider editableUIProvider;
 
   @override
   State<TableComputers> createState() => _TableComputersState();
 }
 
 class _TableComputersState extends State<TableComputers> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    // Simulación de una operación que tarda 2 segundos en completarse
+    Future.delayed(
+        Duration(milliseconds: widget.editableUIProvider.editable ? 50 : 50),
+        () {
+      setState(() {
+        isLoading = false;
+      });
+    });
+  }
+
   List<Widget> getComputers() {
     List<Widget> computerView = [];
 
@@ -23,8 +42,14 @@ class _TableComputersState extends State<TableComputers> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: getComputers(),
-    );
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(
+              color: Colors.blue,
+            ),
+          )
+        : Stack(
+            children: getComputers(),
+          );
   }
 }
