2dd1ef7fb17c9c9359ba07bce52102d89fbca4be
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-10 13:07:57 -0600

    feat: REmove unused call

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:unica_cybercoffee/services/DB/database_static.dart';
 import 'package:unica_cybercoffee/ui/providers/editable_ui_provider.dart';
 import 'package:unica_cybercoffee/ui/providers/theme_provider.dart';
 import 'package:unica_cybercoffee/ui/unica_cybercoffee.dart';
@@ -9,7 +8,6 @@ import 'package:unica_cybercoffee/services/API/states.dart' as states;
 
 void main() async {
   setPathUrlStrategy();
-  await databaseStatic.loadData();
   await onGetStates();
   runApp(const MyApp());
 }
