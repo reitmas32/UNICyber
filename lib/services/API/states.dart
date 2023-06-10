22193542eab3eb5f6e6de6f7a931f2e5d8a3ef70
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-10 12:22:49 -0600

    feat: Create Services by State

---
+++ lib/services/API/states.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:unica_cybercoffee/domain/models/state.dart';
+import 'package:unica_cybercoffee/services/API/data_static.dart';
+
+Future<bool> getStates() async {
+  var url = Uri.parse('http://localhost:3000/api/v1/states');
+  var response = await http.get(url);
+
+  if (response.statusCode == 200) {
+    // La petición fue exitosa, extraer los datos del cuerpo de la respuesta
+    var responseData = jsonDecode(response.body);
+
+    // Acceder a los datos específicos dentro del objeto responseData
+    var success = responseData['Success'];
+
+    if (success) {
+      for (var state in responseData['Data']) {
+        dataStatic.states.add(StateComputer.fromJson(state));
+      }
+    }
+
+    return Future(() => success);
+  }
+
+  return Future(() => false);
+}
+
+Future<bool> setStateOfComputer(int idComputer, int idState) async {
+  var url =
+      Uri.parse('http://localhost:3000/api/v1/computer-set-state/$idComputer');
+  var body = jsonEncode({'id_state': idState});
+  var response = await http.put(url, body: body);
+
+  if (response.statusCode == 200) {
+    // La petición fue exitosa, extraer los datos del cuerpo de la respuesta
+    var responseData = jsonDecode(response.body);
+
+    // Acceder a los datos específicos dentro del objeto responseData
+    var success = responseData['Success'];
+
+    return Future(() => success);
+  }
+
+  return Future(() => false);
+}
