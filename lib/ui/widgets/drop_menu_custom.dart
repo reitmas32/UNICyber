5e2a1e34852799227515ff55c3e47c7fbd0ac9c8
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-11 00:04:01 -0600

    feat: Create COntroller by DropMenu

--- lib/ui/widgets/drop_menu_custom.dart
+++ lib/ui/widgets/drop_menu_custom.dart
@@ -5,41 +5,65 @@ class DropdownMenuCustom extends StatefulWidget {
     super.key,
     required this.items,
     required this.onChanged,
+    required this.controller,
   });
   final List<String> items;
   final void Function(int value) onChanged;
+  final DropMenuController controller;
 
   @override
   DropdownMenuCustomState createState() => DropdownMenuCustomState();
 }
 
 class DropdownMenuCustomState extends State<DropdownMenuCustom> {
-  String _selectedItem = '';
-
   @override
   void initState() {
-    _selectedItem = widget.items.first;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton(
-      borderRadius: BorderRadius.circular(5.0),
-      elevation: 5,
-      hint: const Text('Selecciona una opción'),
-      value: _selectedItem,
-      onChanged: (newValue) {
-        setState(() {
-          _selectedItem = newValue ?? widget.items.first;
-        });
-      },
-      items: widget.items.map((item) {
-        return DropdownMenuItem(
-          value: item,
-          child: Text(item),
-        );
-      }).toList(),
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: Material(
+        elevation: 20,
+        shadowColor: Colors.blueAccent,
+        child: DropdownButton(
+          borderRadius: BorderRadius.circular(5.0),
+          elevation: 5,
+          hint: const Text('Selecciona una opción'),
+          value: widget.items[widget.controller.currentSelected],
+          onChanged: (newValue) {
+            setState(() {
+              widget.controller
+                  .updateMask(widget.items.indexOf(newValue ?? ''));
+            });
+          },
+          items: widget.items.map((item) {
+            return DropdownMenuItem(
+              value: item,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(item),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
+  }
+}
+
+class DropMenuController extends ChangeNotifier {
+  late int currentSelected;
+  DropMenuController() {
+    currentSelected = 0;
+  }
+
+  updateMask(int index) {
+    if (index >= 0) {
+      currentSelected = index;
+      notifyListeners();
+    }
   }
 }
