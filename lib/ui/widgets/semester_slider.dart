bfafdb8bbe3c8d128df6b5cee781cfcef48a40c4
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-11 11:38:53 -0600

    feat: Create Controller by SemesterSlider

--- lib/ui/widgets/semester_slider.dart
+++ lib/ui/widgets/semester_slider.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
 
 class SemesterSlider extends StatefulWidget {
+  final SemesterSliderController controller;
+
+  const SemesterSlider({super.key, required this.controller});
   @override
   _SemesterSliderState createState() => _SemesterSliderState();
 }
 
 class _SemesterSliderState extends State<SemesterSlider> {
-  double _currentSemester = 1;
-
   @override
   Widget build(BuildContext context) {
     return Slider(
       min: 1,
       max: 10,
-      divisions: 7,
-      value: _currentSemester,
+      divisions: 9,
+      value: widget.controller.currentSelected.toDouble(),
       onChanged: (value) {
         setState(() {
-          _currentSemester = value;
+          widget.controller.updateMask(value.toInt());
         });
       },
-      label: _currentSemester.toInt().toString(),
+      label: widget.controller.currentSelected.toString(),
       activeColor: Colors.blue,
       inactiveColor: Color.fromARGB(165, 33, 149, 243),
     );
+  }
+}
+
+class SemesterSliderController extends ChangeNotifier {
+  late int currentSelected;
+  SemesterSliderController() {
+    currentSelected = 1;
+  }
+
+  updateMask(int index) {
+    if (index >= 0) {
+      currentSelected = index;
+      notifyListeners();
+    }
   }
 }
