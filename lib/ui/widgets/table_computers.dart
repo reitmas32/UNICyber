f2cce6d2ee56c0fee9c3476d8a42d411f19e9129
reitmas32 <rafa.zamora.rals@gmail.com>
2023-06-13 07:12:31 -0600

    feat: add lines guide on editable state

--- lib/ui/widgets/table_computers.dart
+++ lib/ui/widgets/table_computers.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:unica_cybercoffee/domain/models/computer.dart';
+import 'package:unica_cybercoffee/ui/providers/editable_ui_provider.dart';
+import 'package:unica_cybercoffee/ui/providers/theme_provider.dart';
 import 'package:unica_cybercoffee/ui/widgets/computer_widget.dart';
 
 class TableComputers extends StatefulWidget {
@@ -25,8 +28,11 @@ class _TableComputersState extends State<TableComputers> {
   }
 
   List<Widget> getComputers() {
+    final editableProvider = Provider.of<EditableUIProvider>(context);
     List<Widget> computerView = [];
-
+    if (editableProvider.editable) {
+      computerView.add(LineGuide());
+    }
     for (var computer in widget.computers) {
       computerView.add(ComputerWidget(computer: computer));
     }
@@ -44,5 +50,59 @@ class _TableComputersState extends State<TableComputers> {
         : Stack(
             children: getComputers(),
           );
+  }
+}
+
+class LineGuide extends StatelessWidget {
+  const LineGuide({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final currentTheme = Provider.of<ThemeProvider>(context);
+
+    return CustomPaint(
+      painter: TablePainter(
+          lineSpacing: 50.0, columnSpacing: 50.0, themeProvider: currentTheme),
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+      ),
+    );
+  }
+}
+
+class TablePainter extends CustomPainter {
+  final double lineSpacing;
+  final double columnSpacing;
+  final ThemeProvider themeProvider;
+
+  TablePainter(
+      {required this.lineSpacing,
+      required this.columnSpacing,
+      required this.themeProvider});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = themeProvider.isDarkTheme() ? Colors.white : Colors.black
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.stroke;
+
+    // Líneas horizontales
+    for (double y = 0; y < size.height; y += lineSpacing) {
+      canvas.drawLine(Offset(0, y), Offset(size.width, y), paint);
+    }
+
+    // Líneas verticales
+    for (double x = 0; x < size.width; x += columnSpacing) {
+      canvas.drawLine(Offset(x, 0), Offset(x, size.height), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
